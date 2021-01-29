dd2fecac715acb98170e4596d840622a49bfbf8a
Yannick-Z <64752052+Yannick-Z@users.noreply.github.com>
2021-01-29 16:56:29 +0100

    kkk

---
+++ react/warzone/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header'
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <section style={{width:"100%", height:"90vh", backgroundColor: 'grey'}}>
+
+    </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/warzone/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+return (
+<header>
+    <span>Logo</span>
+    <h1>Bienvenue sur le pokedex</h1>
+    <span>Menu</span>
+</header>
+)}
+
+export default Header;
